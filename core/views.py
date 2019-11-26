0bfe5f993718cb5197694a216077c3dcc84e38fb
wayne <you@example.com>
2019-11-26 02:13:40 -0600

    added starter data models, home template, style files from MDB

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Item, Order, OrderItem
 
 # Create your views here.
+
+
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "home-page.html", context)
